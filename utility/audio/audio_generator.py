8f9e265138c46b77da48a65cea08a386cf440ae0
rajamanikam23m <rajamanikamm@outlook.com>
2024-09-07 15:20:32 +0530

    Updated Voice to en-US-AriaNeural | audio_generator.py

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,10 +1,5 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+    communicate = edge_tts.Communicate(text,"en-US-AriaNeural")
     await communicate.save(outputFilename)
-
-
-
-
-
