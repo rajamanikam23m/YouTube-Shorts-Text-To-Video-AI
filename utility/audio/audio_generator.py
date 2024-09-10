262ad40afa609bf7aee1c555933e0c6b45de6684
rajamanikam23m <rajamanikamm@outlook.com>
2024-09-10 15:57:06 +0530

    Updated voice to ChristopherNeural - audio_generator.py

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,5 +1,5 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-US-AnaNeural")
+    communicate = edge_tts.Communicate(text,"en-US-ChristopherNeural")
     await communicate.save(outputFilename)
