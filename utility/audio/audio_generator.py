cfc6c06b55360f5102e87965783c1deac09dca7e
rajamanikam23m <rajamanikamm@outlook.com>
2024-09-10 15:16:34 +0530

    Updated audio to AnaNeural - audio_generator.py

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,5 +1,5 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-US-AriaNeural")
+    communicate = edge_tts.Communicate(text,"en-US-AnaNeural")
     await communicate.save(outputFilename)
